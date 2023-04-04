0b5c010649baf00379bce50f18405a0567b474b2
Bifeldy <bifeldy@gmail.com>
2023-04-04 10:19:36 +0700

    Initial commit

--- ObsUtilGUI/Properties/AssemblyInfo.cs
+++ ObsUtilGUI/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ObsUtilGUI")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("https://github.com/bifeldy/obsUtilGUI")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("PT. Indomarco Prismatama")]
 [assembly: AssemblyProduct("ObsUtilGUI")]
 [assembly: AssemblyCopyright("Copyright © 2023 - Bifeldy")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("Bifeldy")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.3")]
+[assembly: AssemblyFileVersion("1.0.0.3")]
